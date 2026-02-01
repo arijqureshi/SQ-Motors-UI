50824b6190c0979557639bbaa5e2f387add4095c
neelpatil <neelpatil@gmail.com>
2026-02-01 12:29:27 -0500

    Routes + Demo Contact Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero';
+import About from '../components/About';
+import WarrantySection from '../components/WarrantySection';
+import MobileFeatures from '../components/MobileFeatures';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <MobileFeatures />
+      <About />
+      <WarrantySection />
+    </>
+  );
+};
+
+export default Home;
